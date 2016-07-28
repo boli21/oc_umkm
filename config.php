c73576b0f9afc2478b630dbc8dd3f18ad575ceac
boli21 <gallazulhy@gmail.com>
2016-07-28 12:01:16 +0700

    Commit Pertama, UMKM Versi 0.1

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/remaster/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/remaster/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/remaster/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/remaster/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/remaster/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/remaster/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/remaster/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/remaster/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/remaster/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/remaster/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/remaster/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/remaster/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/remaster/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'remaster');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
